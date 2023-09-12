f2b8165d5071d3c8178e10531800c427202ff2c1
martindipeco <martindipeco@gmail.com>
2023-09-12 14:43:38 -0300

    primer commit de prueba

---
+++ src/grupo2univesidadejemplo/Grupo2UnivesidadEjemplo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo2univesidadejemplo;
+
+/**
+ *
+ * @author marti
+ */
+public class Grupo2UnivesidadEjemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hola Diana y Martin");
+        
+        System.out.println("5555555fsdfsf");
+        System.out.println("todook");
+    }
+    
+}
